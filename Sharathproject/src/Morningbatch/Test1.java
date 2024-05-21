dc11166a24307a47298142e95b8e6754f72f01b5
sharathkumar25 <168862594+sharathkumar25@users.noreply.github.com>
2024-05-21 22:23:46 +0530

    committed

---
+++ Sharathproject/src/Morningbatch/Test1.java
@@ -0,0 +1,22 @@
+package Morningbatch;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+//		String s1 = "Sharath";
+//        String s2 = new String("Sharath");
+//        String s3 = "   h e l l o S H A R A T H     ";
+//        String s4 = new String("hello");
+//        System.out.println(s1.startsWith("S"));
+//        System.out.println(s1.endsWith("h"));
+//        System.out.println(s1.contains("r"));
+//        System.out.println(s1.length());
+//        System.out.println(s3.length());
+//        System.out.println(s3.trim());
+//        System.out.println(s4.toUpperCase());
+		
+		
+	}
+
+}
